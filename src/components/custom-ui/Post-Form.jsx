8d82c960e775e024bba1cdeb3902379cf539eaed
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-05-02 17:46:20 +0530

    Change the Editor.

--- src/components/custom-ui/Post-Form.jsx
+++ src/components/custom-ui/Post-Form.jsx
@@ -120,15 +120,28 @@ function PostForm({post}) {
                     height: 400,
                     menubar: false,
                     plugins: [
-                      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                      'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                      'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                      "image",
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "charmap",
+                        "preview",
+                        "searchreplace",
+                        "visualblocks",
+                        "code",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                        "code",
+                        "help",
+                        "wordcount",
+                        "anchor"
                     ],
-                    toolbar: 'undo redo | blocks | ' +
-                      'bold italic forecolor | alignleft aligncenter ' +
-                      'alignright alignjustify | bullist numlist outdent indent | ' +
-                      'removeformat | help',
-                    content_style: 'body { font-family: "Noto Sans Devanagari", sans-serif; font-size: 16px; }'
+                    toolbar:
+                    "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                    content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
                   }}
                   onEditorChange={(description) => setValue('description', description)}
                 />
